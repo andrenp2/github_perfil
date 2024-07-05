e4e63443db075627b25f427f75f34db285970e14
Andre Pavan <dehnuernberg@hotmail.com>
2024-07-04 22:03:00 -0300

    ajustante tratamento do erro

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -4,21 +4,36 @@ import styles from './ReposList.module.css';
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [deuErro, setDeuErro] = useState(false);
 
     useEffect(() => {
         setEstaCarregando(true);
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json())
-        .then(resJson => {
+        .then((res) => {
+            if (!res.ok) {
+              throw new Error("Usuário não encontrado");
+            }
+            return res.json();
+          })
+          .then((resJson) => {
+            setTimeout(() => {
+              setEstaCarregando(false);
+              setRepos(resJson);
+            }, 3000);
+          })
+          .catch((e) =>{
             setEstaCarregando(false);
-            setRepos(resJson);
-        })
+            setDeuErro(true);
+            console.error("Erro ao carregar repositórios: ",e.message);
+          });
     }, [nomeUsuario]);
 
     return (
-        <div className="container">
+        <div className="container"> 
             {estaCarregando ? (
                 <h1>Carregando...</h1>
+            ) : deuErro ? (
+                <p>Erro ao carregar os repositórios. Verifique o nome do usuário.</p>
             ) : (
                 <ul className={styles.list}>
                 {repos.map(repositorio => (
